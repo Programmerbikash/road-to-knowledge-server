a060dad7e885b6ff44de70b47a7d2e875dcb2cc4
Programmer Bikash <bikash19081998dey@gmail.com>
2022-11-08 20:18:49 +0600

    crud start

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 // const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
@@ -9,9 +10,27 @@ const app = express();
 app.use(cors()); // For Get Method
 app.use(express.json()); // For Post Method
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// User Name: knowledgeUserDb process.env.DB_USER
+// User Password: LF1520fWLZ7f4JSJ process.env.DB_PASSWORD
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ik3p7tj.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const knowledgeCollection = client.db("knowledge").collection("services");
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = knowledgeCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result)
+          })
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
